ecd63e7c7f53fbb1555f16868c9b03a6240009b2
kroutled <kyle.routledge1@gmail.com>
2018-05-26 16:17:31 +0200

    made some changes

--- main.cpp
+++ main.cpp
@@ -8,14 +8,19 @@ void    ft_createWindow(void)
     cbreak();   //allows signals to be used i.e cntrl Z
     noecho();   //stops charecter input from displaying
     keypad(stdscr, TRUE);   //allows input of keypad and Fkeys
+    curs_set(0);    //sets the cursor to invisible so that we cant see it in window
     box(stdscr, '*', '-');  //generates box border
-    refresh();  //spits out contents of window to the console
-    getch();    //waits for user input
-    endwin();   //clears terminal of curses mode and frees memory
 }
 
 int main(void)
 {
     ft_createWindow();
+    while (1)
+    {
+        getch();
+        refresh();
+        return (0);
+    }
+    endwin();
     return 0;
 }
